23f086bd56a91ede6f459f2908736924a3d315ff
Ricardo Rodriguez <rvrodriguez2000@gmail.com>
2022-07-28 23:00:13 -0700

    changed api and finished project

--- weather.js
+++ weather.js
@@ -1,33 +1,41 @@
+// aip key is imported from this file
+import API_KEY from "./apikey.js";
+
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "a628d50938msh222c8877954209fp19a154jsn2a3d1df8c826",
-    "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
   },
 };
-/*
-fetch("https://open-weather13.p.rapidapi.com/city/chicago", options)
-  .then((response) => response.json())
-  .then((data) => console.log(data.weather[0].main))
-  .catch((err) => console.error(err));
-*/
-
-// Variables
 
 // DOM Elements
 const locationInput = document.getElementById("location-input");
 const weather = document.getElementById("weather");
 const searchButton = document.getElementById("search-button");
+const temp = document.getElementById("temp");
+const city = document.getElementById("city");
 
 searchButton.onclick = () => search(locationInput.value);
 
+//functions
 function search(value) {
-  const city = value.toString();
-  console.log("clicked");
-  fetch(`https://open-weather13.p.rapidapi.com/city/${city}`, options)
+  // fetches data and passes it to getTemeratureResults()
+  fetch(
+    `https://weatherapi-com.p.rapidapi.com/current.json?q=${value}`,
+    options
+  )
     .then((response) => response.json())
-    .then((data) => getTemperatureResults(data.weather[0].main))
+    .then((response) => {
+      getTemperatureResults(response);
+    })
     .catch((err) => console.error(err));
 }
 
-function getTemperatureResults(data) {}
+function getTemperatureResults(data) {
+  //recieves data from search() and displays the data to the DOM
+  console.log(data);
+  city.innerText = `City: ${data.location.name}`;
+  weather.innerText = `Weather: ${data.current.condition.text}`;
+  temp.innerHTML = `Temp: ${data.current.temp_f}<span>&#176;F</span>`;
+}
